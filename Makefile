902c87684a219f14a0af395c1cff9a286424beda
Sandy Carter <bwrsandman@gmail.com>
2013-02-14 13:42:18 -0500

    Replaced application class with .glade file

--- Makefile
+++ Makefile
@@ -3,12 +3,10 @@ CC=g++
 CFLAGS=-Wall `pkg-config --cflags gtkmm-2.4 glibmm-2.4 gtkglextmm-1.2`
 LDFLAGS=`pkg-config --libs gtkmm-2.4 glibmm-2.4 gtkglextmm-1.2`
 
-DEPS= Application.h   \
-	  GLConfigUtil.h  \
+DEPS= GLConfigUtil.h  \
 	  SimpleScene.h
 
-SRCS= Application.cpp \
-	  SimpleScene.cpp \
+SRCS= SimpleScene.cpp \
 	  main.cpp
 
 OBJS= $(SRCS:.cpp=.o)
--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
 /*
  * main.cpp
  * Copyright (C) 2013 Unknown <mr.sandy.carter@gmail.com>
- * 
- * SCarter_engine is free software: you can redistribute it and/or modify it
+ *
+ * SCarter_Engine is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * SCarter_engine is distributed in the hope that it will be useful, but
+ *
+ * SCarter_Engine is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,7 +20,7 @@
 #include <gtkmm.h>
 #include <gtkglmm.h>
 
-#include "Application.h"
+#include "SimpleScene.h"
 
 int main (int argc, char** argv)
 {
@@ -37,8 +37,23 @@ int main (int argc, char** argv)
 			  << major << "." << minor << std::endl;
 
 	/* Instantiate and run the application. */
-	App simple;
-	kit.run(simple);
+	Glib::RefPtr<Gtk::Builder> builder;
+	try {
+		builder = Gtk::Builder::create_from_file("scarter_engine.glade");
+	} catch (const Glib::FileError &ex) {
+		std::cerr << ex.what() << std::endl;
+	}
+
+	Gtk::Window* main_win = NULL;
+	builder->get_widget("main_window", main_win);
+
+	SimpleGLScene* draw = new SimpleGLScene();
+	draw->show();
+	if (main_win)
+	{
+		main_win->add(*draw);
+		kit.run(*main_win);
+	}
 
 	return 0;
 }
