bedf97560c671224d0db0ddadfba159f5729bde2
Sandy Carter <bwrsandman@gmail.com>
2013-02-14 00:06:44 -0500

    formatting

--- SimpleScene.h
+++ SimpleScene.h
@@ -4,18 +4,17 @@
 #include <gtkmm.h>
 #include <gtkglmm.h>
 
-class SimpleGLScene : public Gtk::DrawingArea,
-                      public Gtk::GL::Widget<SimpleGLScene>
+class SimpleGLScene : public Gtk::DrawingArea
+                    , public Gtk::GL::Widget<SimpleGLScene>
 {
 public:
-  SimpleGLScene();
-  virtual ~SimpleGLScene();
+	SimpleGLScene (void);
+	virtual ~SimpleGLScene (void);
 
 protected:
-  virtual void on_realize();
-  virtual bool on_configure_event(GdkEventConfigure* event);
-  virtual bool on_expose_event(GdkEventExpose* event);
-
+	virtual void on_realize (void);
+	virtual bool on_configure_event (GdkEventConfigure* event);
+	virtual bool on_expose_event (GdkEventExpose* event);
 };
 
 #endif
